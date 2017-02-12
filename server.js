8bb09fe9f951207bf7d2d7a7e63cb6af704b3b30
Julien Thoma <julien.thoma89@gmail.com>
2017-02-12 22:41:41 +0100

    port update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1339;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
 const fs = require('fs');
 const crypto = require('crypto');
