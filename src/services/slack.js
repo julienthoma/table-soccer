240cee22b50eb9a3a5a007fe35da75e7f1616b4b
Julien Thoma <julien.thoma89@gmail.com>
2017-07-13 22:39:00 +0200

    slack goal message

--- src/services/slack.js
+++ src/services/slack.js
@@ -45,6 +45,33 @@ export const createStartGameMessage = ({
   ]
 });
 
+export const createGoalMessage = ({
+  goalScorer,
+  color,
+  team1Score,
+  team2Score
+}) => ({
+  username: 'Kicker Bot',
+  attachments: [
+    {
+      author_name: `${goalScorer.name} just scored a Goal`,
+      color,
+      author_link: 'https://example.com',
+      fields: [
+        {
+          value: `_Team 1_ \n *${team1Score}*`,
+          short: true
+        },
+        {
+          value: `_Team 2_ \n *${team2Score}*`,
+          short: true
+        }
+      ],
+      mrkdwn_in: ['author_name', 'fields']
+    }
+  ]
+});
+
 function createPlayerTitle({ name, elo, id }, position) {
   const playerUrl = 'http://example.com';
   return `_${position}_ \n *<${playerUrl}|${name}>* \n _${elo}_`;
