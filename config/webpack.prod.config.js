81b93ca9f267208d6895c963b00a3ec8a912e8aa
Julien Thoma <julien.thoma89@gmail.com>
2018-05-04 08:34:04 +0200

    wip

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -2,26 +2,30 @@ const webpack = require('webpack');
 const path = require('path');
 const config = require('./webpack.base.config');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const APP_DIR = path.resolve(__dirname, '../src');
 const BUILD_DIR = path.resolve(__dirname, '../public');
+const gitRevisionPlugin = new GitRevisionPlugin();
 
 module.exports = Object.assign({}, config, {
   devtool: false,
   entry: {
     app: [`${APP_DIR}/index.js`]
   },
-  output: {
-    path: BUILD_DIR,
-    filename: '[name].min.js'
-  },
   plugins: [
+    new HtmlWebpackPlugin({
+      gitVersion: gitRevisionPlugin.commithash(),
+      template: `${APP_DIR}/index.html`
+    }),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify('production') }
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
       debug: false
-    })
+    }),
+    gitRevisionPlugin
   ],
   optimization: {
     minimizer: [
