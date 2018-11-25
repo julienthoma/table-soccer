23e9779822f5813765927176b8801a6fd41c85f0
Julien Thoma <julien.thoma89@gmail.com>
2018-11-25 17:14:47 +0100

    Bar: use all properties in test

--- src/components/Bar.test.js
+++ src/components/Bar.test.js
@@ -3,6 +3,14 @@ import { renderWithMuiTheme } from '../test.utils';
 import Bar from './Bar';
 
 it('renders correctly', () => {
-  const bar = <Bar leftText="Hello" rightText="World" widthInPercent="50%" />;
+  const bar = (
+    <Bar
+      leftText="Hello"
+      rightText="World"
+      widthInPercent="50%"
+      leftColor="red"
+      rightColor="green"
+    />
+  );
   expect(renderWithMuiTheme(bar)).toMatchSnapshot();
 });
