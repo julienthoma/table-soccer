9b1f9fc750f724cbc9ed8c2467449ce31c259127
Julien Thoma <julien.thoma89@gmail.com>
2019-03-27 22:18:04 +0100

    simplify helper function

--- src/services/Helper.js
+++ src/services/Helper.js
@@ -99,10 +99,4 @@ export const avgOrFallback = (a, b, fallback = 0) => {
   return a / b;
 };
 
-export const avgOrFirstParameter = (a, b) => {
-  if (b === 0 || !b) {
-    return a;
-  }
-
-  return a / b;
-};
+export const avgOrFirstParameter = (a, b) => b === 0 ? a : a / b;
